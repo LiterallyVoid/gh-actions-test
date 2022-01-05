dbbbfeea7d934433805e64b068aa7a542fc59ddd
Literally Void <literallyvoid@gmail.com>
2022-01-04 23:01:05 -0800

    more help

--- .ci/windows.bat
+++ .ci/windows.bat
@@ -2,13 +2,13 @@ echo "Windows run"
 
 if not exist build mkdir build
 
+echo %PATH%
+
+msbuild .ci\windows.project
+
 cd build
 
 cmake ..
 cmake --build .
 
 Debug\all-tests
-
-echo %PATH%
-
-msbuild .ci\windows.project
